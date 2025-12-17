47b7262421b629d6813e86dfa613507f52050155
Marco Vasko-Klima <marcoaklima@gmail.com>
2025-12-17 11:21:11 -0500

    Added Player Movement and Smooth Enemy Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
     public int2 pos;
+    public int2 lastPos;
     public LevelManager manager;
     public float damage;
     public float speed;
@@ -42,31 +44,56 @@ public class Enemy : MonoBehaviour
                 var options = moveLogic.FindNextPos(pos, manager.positions);
                 List<int2> openPoses = new();
 
-                foreach (var option in options)
-                {
-                    if (manager.positions[option.x, option.y].filled == 0)
-                    {
-                        openPoses.Add(option);
-                    }
-                }
+                openPoses = FindOpenPoses(lastPos, options, manager);
 
                 while (openPoses.Count == 0 || PauseMenu.paused)
                 {
                     yield return new WaitForEndOfFrame();
-                    foreach (var option in options)
-                    {
-                        if (manager.positions[option.x, option.y].filled == 0)
-                        {
-                            openPoses.Add(option);
-                        }
-                    }
+                    openPoses = FindOpenPoses(lastPos, options, manager);
                 }
                 manager.positions[pos.x, pos.y].filled = 0;
+                lastPos = pos;
                 pos = openPoses[random.Next(openPoses.Count)];
                 manager.positions[pos.x, pos.y].filled = 1;
                 transform.position = manager.positions[pos.x, pos.y].positionTrans.position;
+
+                float time = 0.0f;
+                float moveTime = moveDelay / 2.0f;
+                while (time < moveTime)
+                {
+                    time += Time.deltaTime;
+                    var prevPos = manager.positions[lastPos.x, lastPos.y].positionTrans.position;
+                    var nextPos = manager.positions[pos.x, pos.y].positionTrans.position;
+                    var stepPos = Vector2.Lerp(prevPos, nextPos, time / moveTime);
+                    transform.position = stepPos;
+                    yield return new WaitForEndOfFrame();
+                }
+                transform.position = manager.positions[pos.x, pos.y].positionTrans.position;
             }
         }
+    }
+
+    static List<int2> FindOpenPoses(int2 lastPos, List<int2> options, LevelManager manager)
+    {
+        List<int2> openPoses = new();
+        foreach (var option in options)
+        {
+            if (manager.positions[option.x, option.y].filled == 0)
+            {
+                openPoses.Add(option);
+            }
+        }
+        if (openPoses.Count > 1)
+        {
+            foreach (var pos in openPoses.ToList())
+            {
+                if (pos.x == lastPos.x && pos.y == lastPos.y)
+                {
+                    openPoses.Remove(pos);
+                }
+            }
+        }
+        return openPoses;
     }
 
     void Attack()
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    Vector2 move;
+    public float3 padding = new(0.1f, 0.8f, 0.1f); // left/right, top, bottom
+    float4 bounds; // left, top, bottom, right
+    float2 dims;
+    public float health = 20.0f;
+
+    void Start()
+    {
+        InitValues();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void InitValues()
+    {
+        float3 margins = new(padding.x * Screen.width, padding.y * Screen.height, padding.z * Screen.height);
+        float4 edges = new(margins.x, Screen.height - margins.y, margins.z, Screen.width - margins.x); // left, top, bottom, right
+        bounds = new(Camera.main.ScreenToWorldPoint(new(edges.x, 0)).x, Camera.main.ScreenToWorldPoint(new(0, edges.y)).y,
+            Camera.main.ScreenToWorldPoint(new(0, edges.z)).y, Camera.main.ScreenToWorldPoint(new(edges.w, 0)).x);
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        dims = new(renderer.bounds.extents.x, renderer.bounds.extents.y);
+    }
+
+    void Move()
+    {
+        transform.position = new(transform.position.x + move.x * moveSpeed * Time.deltaTime, transform.position.y + move.y * moveSpeed * Time.deltaTime);
+        if (transform.position.y + dims.y > bounds.y)
+        {
+            transform.position = new(transform.position.x, bounds.y - dims.y);
+        }
+        else if (transform.position.y - dims.y < bounds.z)
+        {
+            transform.position = new(transform.position.x, bounds.z + dims.y);
+        }
+        if (transform.position.x - dims.x < bounds.x)
+        {
+            transform.position = new(bounds.x + dims.x, transform.position.y);
+        }
+        else if (transform.position.x + dims.x > bounds.w)
+        {
+            transform.position = new(bounds.w - dims.x, transform.position.y);
+        }
+    }
+
+    public void OnMove(InputValue input)
+    {
+        move = input.Get<Vector2>();
+    }
+}
