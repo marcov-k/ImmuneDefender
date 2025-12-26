e49abad82634db4f658c61f93d0bae4003263993
Marco Vasko-Klima <marcoaklima@gmail.com>
2025-12-26 14:08:42 -0500

    Added Note Fading and Dynamic Volumes
    Also added linear music speed increase.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -9,12 +9,16 @@ public class MusicPlayer : MonoBehaviour
     [SerializeField] List<string> tracks = new();
     [SerializeField] List<AudioClip> clips = new();
     readonly Dictionary<string, AudioClip> notesDict = new();
-    readonly Dictionary<string, float> dynDict = new();
+    readonly Dictionary<string, float> dynDict = new() { { "ppp", 1.0f }, { "pp", 2.5f }, { "p", 4.0f }, { "mp", 5.5f }, { "mf", 7.0f },
+        { "f", 8.5f }, { "ff", 10.0f }, { "fff", 11.5f } };
     readonly List<AudioSource> audioSources = new();
     const string trackRegexString = @"(?:\[\((?<note>(?:[A-Z]#?b?[0-9][A-Z])?),(?<time>[0-9\.]*),(?<dyn>[a-z]*)\)\](?<rep>[0-9]*))+?";
     Regex trackRegex;
     readonly List<List<Note>> trackNotes = new();
     public float musicSpeed = 90.0f;
+    public float speedIncrease = 0.1f;
+    public float maxMusicSpeed = 200.0f;
+    public float fadeDuration = 0.25f;
     public float volumeMult = 1.0f;
     float musicVolume = 1.0f;
 
@@ -75,7 +79,7 @@ public class MusicPlayer : MonoBehaviour
                     time = (float)Convert.ToDouble(timeS);
                 }
 
-                var dyn = 5.0f;
+                var dyn = dynDict["mp"];
                 if (dynDict.TryGetValue(dynS, out var noteDyn))
                 {
                     dyn = noteDyn;
@@ -98,6 +102,7 @@ public class MusicPlayer : MonoBehaviour
 
     void StartPlayers()
     {
+        StartCoroutine(SpeedIncrease());
         for (int i = 0; i < trackNotes.Count; i++)
         {
             StartCoroutine(TrackPlayer(trackNotes[i], audioSources[i]));
@@ -118,9 +123,28 @@ public class MusicPlayer : MonoBehaviour
                 source.volume = note.dyn * musicVolume;
                 noteTime = note.time * timeScale;
                 source.Play();
-                yield return new WaitForSecondsRealtime(noteTime);
+                yield return new WaitForSecondsRealtime(noteTime * (1.0f - fadeDuration));
+                float time = 0;
+                float fadeTime = noteTime * fadeDuration;
+                float startVol = source.volume;
+                while (time < fadeTime)
+                {
+                    source.volume = Mathf.Lerp(startVol, 0.0f, time / fadeTime);
+                    time += Time.deltaTime;
+                    yield return new WaitForEndOfFrame();
+                }
                 source.Stop();
             }
+        }
+    }
+
+    IEnumerator SpeedIncrease()
+    {
+        WaitForSeconds delay = new(1.0f);
+        while (musicSpeed < maxMusicSpeed)
+        {
+            musicSpeed = Mathf.Min(musicSpeed + speedIncrease, maxMusicSpeed);
+            yield return delay;
         }
     }
 
